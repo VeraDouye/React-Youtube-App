94db84760cff519fd66bfbaed51100793e977f76
vera douye <veralloydekpebu@gmail.com>
2020-12-16 05:37:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
 function App() {
+  const [query, setQuery] = React.useState('European history');
+  const [list, setList] = React.useState(null);
+
+  const search = (e) => {
+    e.preventDefault();
+    searchYouTube(query).then(setList);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <form onSubmit={search}>
+        <input autoFocus value={query} onChange={e => setQuery(e.target.value)} />
+        <button>Search YouTube</button>
+      </form>
+      {list &&
+        (list.length === 0
+          ? <p>No results</p>
+          : (
+            <ul className="items">
+              {list.map(item => (
+                <li className="item" key={item.id}>
+                  <div>
+                    <b><a href={item.link}>{item.title}</a></b>
+                    <p>{item.description}</p>
+                  </div>
+                  <ul className="meta">
+                    <li>By: <a href={item.author.ref}>{item.author.name}</a></li>
+                    <li>Views: {item.views}</li>
+                    <li>Duration: {item.duration}</li>
+                    <li>Uploaded: {item.uploaded_at}</li>
+                  </ul>
+                  <img alt="" src={item.thumbnail} />
+                </li>
+              ))}
+            </ul>
+          )
+        )
+      }
     </div>
   );
+}
+
+async function searchYouTube(q) {
+  q = encodeURIComponent(q);
+  const response = await fetch("https://youtube-search-results.p.rapidapi.com/youtube-search/?q=" + q, {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "youtube-search-results.p.rapidapi.com",
+      "x-rapidapi-key": "af2d7a3c9dmsh409c3d81d409dadp1309b0jsn5675e9e875ca"
+    }
+  });
+  const body = await response.json();
+  console.log(body);
+  return body.items.filter(item => item.type === 'video');
 }
 
 export default App;
